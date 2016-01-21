3e9751d477d79b430e7c4d088f6c0afac86556b6
Julian Lee <Julian@julians-macbook-pro.local>
2016-01-21 15:47:17 -0800

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Julian Lee on 1/21/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Julian Lee on 1/21/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
